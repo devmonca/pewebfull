a04e41f3ff2d07742009a2e7bc6c988093a1ed75
gabriel <devmonca@gmail.com>
2025-02-11 23:18:05 -0300

    Project created

---
+++ src/main/java/com/devmonca/pewebfull/PewebfullApplication.java
@@ -0,0 +1,13 @@
+package com.devmonca.pewebfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PewebfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PewebfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmonca/pewebfull/PewebfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmonca.pewebfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PewebfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
